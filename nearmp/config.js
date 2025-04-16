ea9a916fcb9d423f9e3b61c4126edbd3d1a4f257
Joe S <DcJoeS@users.noreply.github.com>
2025-04-15 20:23:08 -0500

    nearmap example

---
+++ nearmp/config.js
@@ -0,0 +1,13 @@
+const DEMO_API_KEY = 'NzU2YzM2NWQtMWQwOC00OWU3LWJjZDctYjRkNTg2MzllODZh';
+
+const CENTER = [138.595637, -34.915302];
+
+const MIN_ZOOM = 0;
+
+const MAX_ZOOM = 24;
+
+const ZOOM = 13;
+
+let SINCE = "";
+
+let UNTIL = "";

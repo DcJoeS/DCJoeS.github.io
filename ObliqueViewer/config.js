2439a6263ccc9c2be6b94c58abf1b8b6afa60cfe
Joe S <DcJoeS@users.noreply.github.com>
2025-04-25 09:38:23 -0500

    renamed and started new test

---
+++ ObliqueViewer/config.js
@@ -0,0 +1,13 @@
+const DEMO_API_KEY = 'NzU2YzM2NWQtMWQwOC00OWU3LWJjZDctYjRkNTg2MzllODZh';
+
+const CENTER = [-93.209488, 44.733173];
+
+const MIN_ZOOM = 0;
+
+const MAX_ZOOM = 24;
+
+const ZOOM = 13;
+
+let SINCE = "";
+
+let UNTIL = "";
